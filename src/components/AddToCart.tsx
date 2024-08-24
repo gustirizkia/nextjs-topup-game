7340792c3b70e5d767c2d29f47acf48932425b78
Gusti Maulana Yokesen <gustimaulana.yokesen@gmail.com>
2024-08-25 03:31:35 +0700

    delete add cart

--- src/components/AddToCart.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useCartStore } from "@/zustand/Cart";
-
-export const handleSelectData = (item: any) => {
-  const storeCart = useCartStore();
-  console.log("item", item);
-};
